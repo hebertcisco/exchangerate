9e7ce893b8a849da66e5fa7628ad1b3321442a4c
hebertcisco <mrrabbitomg@gmail.com>
2021-01-17 11:10:26 -0300

    created: exchange Rates job

---
+++ src/jobs/exchangerate.job.js
@@ -0,0 +1,26 @@
+const CronJob = require("cron").CronJob;
+const exchangeRateModel = require("../database/schemas/ExchangeRateModel");
+const { exchangeRates } = require("exchange-rates-api");
+
+module.exports = new CronJob(
+  "0 0 0 */1 * *",
+  async () => {
+    const { CAD, BRL, EUR, USD } = await exchangeRates()
+      .latest()
+      .base("USD")
+      .fetch();
+    console.log("Updating...");
+    await exchangeRateModel.findOneAndUpdate(
+      { _id: process.env.MONGODB_TABLE },
+      {
+        CAD,
+        BRL,
+        EUR,
+        USD,
+      }
+    );
+  },
+  null,
+  true,
+  "America/Sao_Paulo"
+);
