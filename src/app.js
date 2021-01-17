bd45ad84790a8ac4fac9318cf39e9cad1932dd28
hebertcisco <mrrabbitomg@gmail.com>
2021-01-17 10:58:59 -0300

    created: app file configuration

---
+++ src/app.js
@@ -0,0 +1,18 @@
+require("dotenv/config");
+
+const express = require("express");
+const cors = require("cors");
+
+const routes = require("./routes");
+
+require("./jobs/exchangerate.job");
+
+require("./database");
+
+const app = express();
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(routes);
+
+module.exports = app;
